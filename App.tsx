976accabc47a0bcb97a10988ed2c1d29d6f152d2
Willian Barreto Froes <willian.froes@pampa.compasso>
2022-02-10 16:13:38 -0300

    coupon input in cart screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import 'react-native-gesture-handler';
-import { NavigationContainer, TypedNavigator } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import MainView from './assets/view/MainView';
@@ -13,7 +13,7 @@ export default function App() {
   return(
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Main" component={MainView} />
+        {/* <Stack.Screen name="Main" component={MainView} /> */}
         <Stack.Screen name="Order" component={OrderView} />
       </Stack.Navigator>
     </NavigationContainer>
--- assets/view/OrderView.tsx
+++ assets/view/OrderView.tsx
@@ -1,18 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
-
 import { Text, View, StyleSheet } from 'react-native';
+
+import InputWithButton from '../component/InputWithButton';
 
 interface Props {
     navigation: StackNavigationProp<any,any>
 }
 
 const MainView: React.FC<Props> = ({ navigation }) => {
+    const [couponText, SetCouponText] = useState<string>("");
+
     return (
         <View style={styles.container}>
             <StatusBar style='dark' backgroundColor='#ffffff' translucent={false} />
 
-            <Text>Products Cart Screen</Text>
+            <InputWithButton 
+                callableMethod={() => {
+                    console.log("coupon!");
+                }}
+                callableCancelMethod={()=> {
+                    SetCouponText("");
+                }}
+                inputPlaceholder={"Have coupon? Insert here!"}
+                buttonIcon={require("../image/check-coupon-icon.png")}
+                callableSetter={SetCouponText}
+                value={couponText}
+            />
         </View>
     );
 }
